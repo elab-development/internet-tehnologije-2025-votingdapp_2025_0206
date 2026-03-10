3ab1c783e23faa0d51e68f6ee7d1af551f9d9d38
DDKM-03 <djole.delijovanis5@gmail.com>
2026-03-10 01:55:29 +0100

    Prosiren test slucajevi za backend

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,10 +1,28 @@
+import os
+
+# Ubacujemo lažne varijable za bazu pre učitavanja
+os.environ["DATABASE_URL"] = "sqlite:///./testbaza.db"
+os.environ["SECRET_KEY"] = "super_tajna_sifra_za_test"
+
 from fastapi.testclient import TestClient
-from app.main import app 
+from app.main import app
 
 client = TestClient(app)
 
-def test_get_topics_without_auth():
+def test_topics_route_protected():
+    "Testiranje bezbednosti - blokiranje neulogovanih korisnika (Status 401)"
+    # Pokušavamo da dohvatimo teme bez JWT tokena
     response = client.get("/topics")
+    # Aplikacija ispravno prepoznaje da nismo ulogovani i blokira pristup
+    assert response.status_code == 401
 
-    assert response.status_code in [200, 401, 403] 
-    print("Backend test uspešno komunicira sa API-jem!")
+def test_me_route_protected():
+    "Testiranje bezbednosti - pristup tuđem profilu (Status 401)"
+    response = client.get("/me")
+    assert response.status_code == 401
+
+def test_login_validation_error():
+    "3. Testiranje Pydantic validacije (Status 422)"
+    # Simuliramo POST zahtev sa praznim/pogrešnim telom umesto adrese novčanika
+    response = client.post("/login", json={"pogresno_polje": "123"})
+    assert response.status_code == 422
